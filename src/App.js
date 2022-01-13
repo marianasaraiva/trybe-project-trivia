ba78a073300191e944a4fb4603a840af07a712bd
RuanCunha <ruanvcunha@hotmail.com>
2022-01-13 17:43:22 -0300

    Setup inicial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import logo from './trivia.png';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
+import Login from './pages/Login';
+import Feedback from './pages/Feedback';
+import Play from './pages/Play';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/feedback" component={ Feedback } />
+          <Route exact path="/play" component={ Play } />
+          <Route exact path="/ranking" component={ Ranking } />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
