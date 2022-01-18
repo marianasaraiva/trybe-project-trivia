c170203d2553ed2d8da34dee388ef9421d567ae8
Mariana Saraiva <marianascm8@gmail.com>
2022-01-18 11:28:28 -0300

    Requisito 17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
-        <h1 data-testid="ranking-title"> Página do ranking</h1>
+        <h1 data-testid="ranking-title">Página do ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => {
+            history.push('/');
+          } }
+        >
+          Play again
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default Ranking;
